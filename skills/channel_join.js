8f78638521092da532779483f08db06349d13bc5
Jamie Warburton <jamiewarb@gmail.com>
2017-07-05 13:56:27 +0100

    Fixed bot

--- skills/channel_join.js
+++ skills/channel_join.js
@@ -6,13 +6,16 @@ module.exports = function(controller) {
 
         bot.startConversation(message, function(err, convo) {
 
-            convo.say(message, 'http://gph.is/2bsaGX6');
-            convo.say(message, 'As a kid, I got three meals a day. Oatmeal, miss-a-meal and no meal');
-            convo.say(message, 'But fine, I\'ll help. Just type let me know what you want to do, or type `help`');
+            convo.say('http://gph.is/2bsaGX6');
+            convo.say('As a kid, I got three meals a day. Oatmeal, miss-a-meal and no meal');
+            convo.say('But fine, I\'ll help. Just type let me know what you want to do, or type `help`');
 
-        // controller.studio.run(bot, 'channel_join', message.user, message.channel).catch(function(err) {
-        //     debug('Error: encountered an error loading onboarding script from Botkit Studio:', err);
-        // });
+            convo.next();
+        });
+
+        controller.studio.run(bot, 'channel_join', message.user, message.channel).catch(function(err) {
+            debug('Error: encountered an error loading onboarding script from Botkit Studio:', err);
+        });
 
     });
 
