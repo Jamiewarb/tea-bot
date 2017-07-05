fbb1179f693716415018bbce1727c9c8d65bb6fc
Jamie Warburton <jamiewarb@gmail.com>
2017-07-05 16:30:37 +0100

    Reverted from interactive message

--- skills/hears_me.js
+++ skills/hears_me.js
@@ -3,43 +3,63 @@
  */
 
 module.exports = function(controller) {
-    controller.hears(['me'], 'direct_message,direct_mention,mention', function(bot, message) {
+    controller.hears(['me'], 'direct_mention,mention', function(bot, message) {
+        console.log(message);
         beginMe(controller, bot, message);
-    });
-
-    controller.on('interactive_message_callback', function(bot, trigger) {
-        if (trigger.actions[0].name.match(/^confirm_me$/)) {
-            bot.reply('Your answer was ' + trigger.actions[0].value);
-            return false;
-        }
     });
 }
 
 const beginMe = function(controller, bot, message) {
-    bot.reply(message, {
-        'text': 'So you\'re going to make a round of tea, are you?',
-        'attachments': [
+
+    bot.createConversation(message, function(err, convo) {
+
+        convo.addMessage({
+            text: 'It won\t be as good as mine, but you gotta start somewhere. I\'ll let everyone know',
+        },'yes_thread');
+
+        convo.addMessage({
+            text: 'Why you wasting my time fool',
+            action: 'stop', // Marks converation as unsuccessful
+        },'no_thread');
+
+        convo.addMessage({
+            text: 'GRRRR! Was that a `yes` or a `no`?',
+        },'bad_response');
+
+        // Create a yes/no question in the default thread...
+        convo.ask('So you\'re going to make a round of tea, are you?', [
             {
-                'fallback': 'Looks like you\'re unable to make tea I\'m afraid',
-                'color': '#f8b88b',
-                'callback_id': 'me',
-                'attachment_type': 'default',
-                'actions': [
-                    {
-                        'name': 'confirm_me',
-                        'text': 'Yes',
-                        'type': 'button',
-                        'value': 'yes'
-                    },
-                    {
-                        'name': 'confirm_me',
-                        'text': 'No',
-                        'type': 'button',
-                        'value': 'no'
-                    },
-                ]
+                pattern:  bot.utterances.yes,
+                callback: function(response, convo) {
+                    convo.gotoThread('yes_thread');
+                },
+            },
+            {
+                pattern:  bot.utterances.no,
+                callback: function(response, convo) {
+                    convo.gotoThread('no_thread');
+                },
+            },
+            {
+                default: true,
+                callback: function(response, convo) {
+                    convo.gotoThread('bad_response');
+                },
             }
-        ]
+        ]);
+
+        convo.activate();
+
+        convo.on('end', function(convo) {
+
+            if (convo.successful()) {
+                bot.reply(message, 'Okay, off I go!!!');
+
+                // and now deliver cheese via tcp/ip...
+
+            }
+
+        });
     });
 }
 
