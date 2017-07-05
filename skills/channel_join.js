b5e3bfed316a4fe7b42afdcb7e67b5250bdaf8d1
Jamie Warburton <jamiewarb@gmail.com>
2017-07-05 13:54:39 +0100

    Added channel join message

--- skills/channel_join.js
+++ skills/channel_join.js
@@ -3,11 +3,16 @@ var debug = require('debug')('botkit:channel_join');
 module.exports = function(controller) {
 
     controller.on('bot_channel_join', function(bot, message) {
-        console.log("BORK");
 
-        controller.studio.run(bot, 'channel_join', message.user, message.channel).catch(function(err) {
-            debug('Error: encountered an error loading onboarding script from Botkit Studio:', err);
-        });
+        bot.startConversation(message, function(err, convo) {
+
+            convo.say(message, 'http://gph.is/2bsaGX6');
+            convo.say(message, 'As a kid, I got three meals a day. Oatmeal, miss-a-meal and no meal');
+            convo.say(message, 'But fine, I\'ll help. Just type let me know what you want to do, or type `help`');
+
+        // controller.studio.run(bot, 'channel_join', message.user, message.channel).catch(function(err) {
+        //     debug('Error: encountered an error loading onboarding script from Botkit Studio:', err);
+        // });
 
     });
 
