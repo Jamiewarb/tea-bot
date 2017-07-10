378b19383d877b6883e5d611cb3df679c29b2493
Jamie Warburton <jamiewarb@gmail.com>
2017-07-10 13:52:06 +0100

    Fixed bug with manualAdd commands

--- skills/manual_stat_adjustment.js
+++ skills/manual_stat_adjustment.js
@@ -1,3 +1,4 @@
+var config = require('../config.js');
 var tracking = require('../shared/tracking.js');
 var statistics = require('./statistics.js');
 
@@ -10,6 +11,9 @@ module.exports = function(controller) {
         if (message.text.indexOf(' ') > 0) {
             let userName = message.text.substr(6);
             bot.api.users.info({user: userName}, (err, response) => {
+                if (response.user == null) {
+                    bot.reply(message, 'Could not find user with id ' + userName);
+                }
                 let {id, name} = response.user;
                 bot.reply(message, '```' + id + ' : ' + name + '```');
             });
@@ -30,25 +34,39 @@ module.exports = function(controller) {
         });
     });
 
-    controller.hears(['^manualAddDrank'], 'direct_message', function(bot, message) {
-        let tokens = message.text.split(' ');
-        if (tokens[1] !== config.settings.admin) return;
-        if ((typeof tokens[2] === 'string') && Number.isInteger(tokens[3])) {
-            statistics.addDrank(tokens[2], tokens[3]);
-            bot.reply(message, 'User <@' + tokens[2] + '> has been added ' + tokens[3] + ' drank drinks');
-        } else {
-            bot.reply(message, 'This command must be in the form "manualAddDrank" <password> <userID> <amount>');
-        }
+    controller.hears(['^manualAddMade'], 'direct_message', function(bot, message) {
+        praseManualAdd(bot, message, 'made');
     });
 
-    controller.hears(['^manualAddMade'], 'direct_message', function(bot, message) {
-        let tokens = message.text.split(' ');
-        if (tokens[1] !== config.settings.admin) return;
-        if ((typeof tokens[2] === 'string') && Number.isInteger(tokens[3])) {
-            statistics.addMade(tokens[2], tokens[3]);
-            bot.reply(message, 'User <@' + tokens[2] + '> has been added ' + tokens[3] + ' made drinks');
-        } else {
-            bot.reply(message, 'This command must be in the form "manualAddMade" <password> <userID> <amount>');
-        }
+    controller.hears(['^manualAddDrank'], 'direct_message', function(bot, message) {
+        praseManualAdd(bot, message, 'drank');
     });
+
+    function parseManualAdd(bot, message, mode) {
+        let tokens = message.text.split(' '),
+            invalid = false;
+        if (tokens[1] !== config.settings.admin) return;
+        tokens[3] = parseInt(tokens[3]);
+        if ((typeof tokens[2] !== 'string') || (!Number.isInteger(tokens[3]))) {
+            invalid = 'true';
+        }
+        switch (mode) {
+            case 'made':
+                if (invalid) {
+                    bot.reply(message, 'This command must be in the form "manualAddMade" <password> <userID> <amount>');
+                    return;
+                }
+                statistics.addMade(tokens[2], tokens[3]);
+                bot.reply(message, 'User <@' + tokens[2] + '> has been added ' + tokens[3] + ' made drinks');
+                break;
+            case 'drank':
+                if (invalid) {
+                    bot.reply(message, 'This command must be in the form "manualAddDrank" <password> <userID> <amount>');
+                    return;
+                }
+                statistics.addDrank(tokens[2], tokens[3]);
+                bot.reply(message, 'User <@' + tokens[2] + '> has been added ' + tokens[3] + ' drank drinks');
+                break;
+        }
+    }
 }
