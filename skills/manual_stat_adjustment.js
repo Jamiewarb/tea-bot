0b952ee46553f258c92b98996c64a1df34bb5461
Jamie Warburton <jamiewarb@gmail.com>
2017-07-10 13:52:06 +0100

    Fixed bug with manualAdd commands

--- skills/manual_stat_adjustment.js
+++ skills/manual_stat_adjustment.js
@@ -1,3 +1,4 @@
+var config = require('../config.js');
 var tracking = require('../shared/tracking.js');
 var statistics = require('./statistics.js');
 
@@ -10,6 +11,9 @@ module.exports = function(controller) {
         if (message.text.indexOf(' ') > 0) {
             let userName = message.text.substr(6);
             bot.api.users.info({user: userName}, (err, response) => {
+                if (response.user == null) {
+                    bot.reply(message, 'Could not find user with id ' + userName);
+                }
                 let {id, name} = response.user;
                 bot.reply(message, '```' + id + ' : ' + name + '```');
             });
@@ -42,12 +46,19 @@ module.exports = function(controller) {
     });
 
     controller.hears(['^manualAddMade'], 'direct_message', function(bot, message) {
+        console.log("Heard");
         let tokens = message.text.split(' ');
+        console.log("Split");
         if (tokens[1] !== config.settings.admin) return;
+        console.log("Admin Password Succeeded");
         if ((typeof tokens[2] === 'string') && Number.isInteger(tokens[3])) {
+            console.log("Correct Types");
             statistics.addMade(tokens[2], tokens[3]);
+            console.log("AddMade called");
             bot.reply(message, 'User <@' + tokens[2] + '> has been added ' + tokens[3] + ' made drinks');
+            console.log("Bot Reply done");
         } else {
+            console.log("Bot Reply incorrect format");
             bot.reply(message, 'This command must be in the form "manualAddMade" <password> <userID> <amount>');
         }
     });
