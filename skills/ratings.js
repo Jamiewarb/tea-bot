365a564a9550a05e6b2955b0f0633e9275c608bc
Jamie Warburton <jamiewarb@gmail.com>
2017-08-10 17:20:05 +0100

    Prevented users not in the round from voting

--- skills/ratings.js
+++ skills/ratings.js
@@ -6,16 +6,27 @@ module.exports = function(controller) {
     });
 
     controller.on('interactive_message_callback', function(bot, trigger) {
-
         let callback_id = trigger.callback_id;
         if (callback_id.startsWith('rating_')) {
             if (callback_id.indexOf('_') > 0) {
-                let roundID = callback_id.substr(callback_id.indexOf('_') + 1);
+                let roundID = callback_id.substr(callback_id.indexOf('_') + 1),
+                    rating = false;
+
+                console.log(trigger.actions[0].value['allUsers']);
+
                 if (trigger.actions[0].name === 'rateUp') {
-                    statistics.rateBrew(bot, trigger.team.id, trigger.actions[0].value, trigger.user, 'up', roundID);
+                    rating = 'up';
+                } else if (trigger.actions[0].name === 'rateDown') {
+                    rating = 'down';
                 }
-                if (trigger.actions[0].name === 'rateDown') {
-                    statistics.rateBrew(bot, trigger.team.id, trigger.actions[0].value, trigger.user, 'down', roundID);
+                if (rating) {
+                    statistics.rateBrew(bot,
+                                        trigger.team.id,
+                                        trigger.actions[0].value['maker'],
+                                        trigger.user,
+                                        rating,
+                                        roundID,
+                                        trigger.actions[0].value['allUsers']);
                 }
             }
         }
